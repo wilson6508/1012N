188ebf56ee46f564e7396ee6f1d5678e8a99d61e
wilson6508 <wilson650837@gmail.com>
2024-04-07 22:02:26 +0800

    init

---
+++ 0_BjNode/P5_MyTest.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.test;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// 上方運行
+		// 滑鼠右鍵運行
+		
+		// 滑鼠右鍵Debug運行
+		System.out.println("Hello World 123");
+		System.out.println("Hello World 456");
+		// syso + (alt + /)		
+	}
+
+}
